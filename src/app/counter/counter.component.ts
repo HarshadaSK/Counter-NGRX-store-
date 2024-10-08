0aec2279009c5db3a9f033cbe08171da5aade242
Harshada kattewar <kattewarharshada@gmail.com>
2024-10-08 16:02:39 +0530

    first commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,39 @@
+import { AsyncPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import { selectCount } from '../store/counter.selector';
+import { AppState } from '../store/app.state';
+import { decrement, increment, reset } from '../store/counter.action';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+})
+
+export class CounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select(selectCount);
+  }
+
+  increaseCount() {
+    this.store.dispatch(increment());
+  }
+  decreaseCount() {
+    this.count$.pipe(take(1)).subscribe((count) => {
+      if (count > 0) {
+        this.store.dispatch(decrement());
+      } else {
+        alert('Counter can not be less than 0');
+      }
+    });
+  }
+  resetCount() {
+    this.store.dispatch(reset());
+  }
+}
